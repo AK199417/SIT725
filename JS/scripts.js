068fa9c6587382b0360e5938e3d36182c8856af5
AK199417 <arulrajah.kumaraguruparar@gmail.com>
2025-03-12 20:10:48 +1100

    My final code

---
+++ JS/scripts.js
@@ -0,0 +1,28 @@
+function changeText() {
+    var textsArray = ["Green", "Blue", "Yellow", "Red", "Grey"];  // Color names as text
+    var colors = ["Reg", "Orange", "yellow", "red", "grey"];  // Matching color values
+    
+    // Loop through the textsArray and choose a random index
+    for (let i = 0; i < textsArray.length; i++) {
+        // Generate a random number between 0 and textsArray.length - 1
+        var randomIndex = getRandomNumberBetween(0, textsArray.length - 1);
+        
+        // Get the color text and color name
+        var colorText = textsArray[randomIndex];
+        var color = colors[randomIndex];
+        if (color = colorText){        // Matching the color
+        var heading = document.getElementById("heading");
+        }
+        // Set the text and color
+        heading.innerHTML = colorText; // Set the text (color name)
+        heading.style.color = color; // Set the color
+
+    
+
+    }
+}
+  
+  function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
